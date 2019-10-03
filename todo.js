3e0471a0fe1782d41f9ba04420fbf13a5f002280
Qasem <qaseem.com34@gmail.com>
2019-10-03 01:09:45 -0700

    update on javaScript

--- todo.js
+++ todo.js
@@ -1,33 +1,43 @@
+const todos = [];
+
 document.getElementById("txt").addEventListener("input", function() {
     document.getElementById("plus").style.visibility = "visible";
 });
 
-function listItem() {
-    let list = document.createElement("li");
-    let textlist = document.getElementById("txt").value;
-    let ul = document.getElementById("ulli");
-    list.appendChild(document.createTextNode(textlist));
-    ul.appendChild(list);
-    document.getElementById("txt").value = "";
-    document.getElementById("cont").style.display = "block";
+function addTodo(text) {
+    todos.push({
+        text: text,
+        isCompleted: false
+    });
+}
 
-    var liList = document.getElementById("ulli").getElementsByTagName("li");
-    document.getElementById("count").innerHTML = liList.length;
+function renderTodos(todos) {
+    const html = todos.map(todo => {
+        const circleFileName = todo.isCompleted ? 'check_circle_outline.png' : 'fiber_manual_record-24px.png';
+        return '<div class="todo">' +
+            '<img id="clo" class="clo" src="' + circleFileName + '">' +
+            '<span>' + todo.text + '</span>' +
+            '<img id="clo" class="clo" src="cancel-24px.png">' +
+            '</div>';
+    });
 
-    list.onclick = removeItem;
-
-    function removeItem(del) {
-        del.target.parentElement.removeChild(del.target);
-    }
+    document.getElementById('todos').innerHTML = html.join('');
 }
 
 document.getElementById("txt").addEventListener("keypress", function(ent) {
-    (ent.keyCode === 13 ? listItem() : "")
+    if (ent.keyCode === 13) {
+        addTodo(
+            document.getElementById("txt").value
+        );
+
+        renderTodos(todos);
+    }
+
     document.getElementById("plus").style.visibility = "hidden";
 });
 
 document.getElementById("plus").addEventListener("click", function() {
-    (document.getElementById("txt").value ? listItem() : "")
+    (document.getElementById("txt").value ? addTodo() : "")
     document.getElementById("plus").style.visibility = "hidden";
 
 });
