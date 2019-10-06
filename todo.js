ebfaadc5403ac8439c3c63024531b366ce0f3d02
Qasem <qaseem.com34@gmail.com>
2019-10-05 22:47:04 -0700

    edit on edit button

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const todos = [];
 
-document.getElementById("txt").addEventListener("input", function() {
+document.getElementById("txt").addEventListener("input", function () {
     document.getElementById("plus").style.visibility = "visible";
 });
 
@@ -8,12 +8,12 @@ function addTodo(text) {
     todos.push({
         text: text,
         isCompleted: false
+
     });
 }
-
 function renderTodos(todos) {
     const html = todos.map(todo => {
-        const circleFileName = todo.isCompleted ? 'check_circle_outline.png' : 'fiber_manual_record-24px.png';
+        const circleFileName = todo.isCompleted ? 'check.png' : 'circle.png';
         return '<div class="todo">' +
             '<img id="cl" class="cl" src="' + circleFileName + '">' +
             '<span id="span-txt">' + todo.text + '</span>' +
@@ -22,9 +22,11 @@ function renderTodos(todos) {
     });
 
     document.getElementById('todos').innerHTML = html.join('');
+    document.getElementById("count").innerHTML=todos.length;
+    
 }
 
-document.getElementById("txt").addEventListener("keypress", function(ent) {
+document.getElementById("txt").addEventListener("keypress", function (ent) {
     if (ent.keyCode === 13) {
         addTodo(
             document.getElementById("txt").value
@@ -38,10 +40,10 @@ document.getElementById("txt").addEventListener("keypress", function(ent) {
 
 });
 
-
-
-document.getElementById("plus").addEventListener("click", function() {
-    (document.getElementById("txt").value ? addTodo() : "")
+document.getElementById("plus").addEventListener("click", function () {
+    (document.getElementById("txt").value ? addTodo(document.getElementById("txt").value) : "");
+    renderTodos(todos);
     document.getElementById("plus").style.visibility = "hidden";
 
 });
+
