895267cb24866d938576386ea919eee2ea8cfeb6
Qasem <qaseem.com34@gmail.com>
2019-10-06 03:28:30 -0700

    edait on all

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const todos = [];
 
-document.getElementById("txt").addEventListener("input", function () {
+document.getElementById("txt").addEventListener("input", function() {
     document.getElementById("plus").style.visibility = "visible";
 });
 
@@ -11,39 +11,53 @@ function addTodo(text) {
 
     });
 }
+
 function renderTodos(todos) {
-    const html = todos.map(todo => {
+    const html = todos.map((todo, index) => {
         const circleFileName = todo.isCompleted ? 'check.png' : 'circle.png';
+
         return '<div class="todo">' +
-            '<img id="cl" class="cl" src="' + circleFileName + '">' +
+            '<img id="" class="cl" src="' + circleFileName + '">' +
             '<span id="span-txt">' + todo.text + '</span>' +
-            '<img id="clo" class="clo" src="cancel-24px.png">' +
+            '<img id="delete-' + index + '" class="clo" src="cancel-24px.png" data-id="' + index + '" onClick="deleteTodo(event)">' +
             '</div>';
     });
 
     document.getElementById('todos').innerHTML = html.join('');
-    document.getElementById("count").innerHTML=todos.length;
-    
+    document.getElementById("count").innerHTML = todos.length;
+    document.getElementById("txt").value = "";
+    document.getElementById("plus").style.visibility = "hidden";
 }
 
-document.getElementById("txt").addEventListener("keypress", function (ent) {
+function deleteTodo(event) {
+    let hr = getElementById("delete-");
+    Console.log("hr");
+
+}
+
+document.getElementById("txt").addEventListener("keypress", function(ent) {
     if (ent.keyCode === 13) {
-        addTodo(
-            document.getElementById("txt").value
-        );
+        const text = document.getElementById("txt").value;
+
+        if (text === '') {
+            return;
+        }
+
+        addTodo(text);
 
         renderTodos(todos);
-        document.getElementById("txt").value = "";
+    }
+});
+
+document.getElementById("plus").addEventListener("click", function() {
+    const text = document.getElementById("txt").value;
+
+    if (text === '') {
+        return;
     }
 
-    document.getElementById("plus").style.visibility = "hidden";
+    addTodo(text);
 
-});
-
-document.getElementById("plus").addEventListener("click", function () {
-    (document.getElementById("txt").value ? addTodo(document.getElementById("txt").value) : "");
     renderTodos(todos);
-    document.getElementById("plus").style.visibility = "hidden";
 
 });
-
