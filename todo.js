6313f1c82d8cd0e01cd4a2ab26cbfe3c9363725c
Qasem <qaseem.com34@gmail.com>
2019-10-07 05:16:14 -0700

    eadit all

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-const todos = [];
+let todos = [];
 
 document.getElementById("txt").addEventListener("input", function() {
     document.getElementById("plus").style.visibility = "visible";
@@ -17,9 +17,9 @@ function renderTodos(todos) {
         const circleFileName = todo.isCompleted ? 'check.png' : 'circle.png';
 
         return '<div class="todo">' +
-            '<img id="" class="cl" src="' + circleFileName + '">' +
+            '<img class="cl" src="' + circleFileName + '" data-id="' + index + '" onClick="toggleCheck(event)">' +
             '<span id="span-txt">' + todo.text + '</span>' +
-            '<img id="delete-' + index + '" class="clo" src="cancel-24px.png" data-id="' + index + '" onClick="deleteTodo(event)">' +
+            '<img class="clo" src="cancel-24px.png" data-id="' + index + '" onClick="deleteTodo(event)">' +
             '</div>';
     });
 
@@ -28,17 +28,6 @@ function renderTodos(todos) {
     document.getElementById("txt").value = "";
     document.getElementById("plus").style.visibility = "hidden";
 }
-
-// function deleteTodo(event) {
-//     for (var i = todos.length - 1; i >= 0; i--) {
-//         if (todos[i] === todos) {
-//             todos.splice(i, 0);
-
-//         }
-//         delete todos[i];
-//     }
-
-// }
 
 document.getElementById("txt").addEventListener("keypress", function(ent) {
     if (ent.keyCode === 13) {
@@ -67,15 +56,20 @@ document.getElementById("plus").addEventListener("click", function() {
 
 });
 
+function toggleCheck(event) {
+    const index = event.target.getAttribute('data-id');
+    todos[index].isCompleted = !todos[index].isCompleted;
+    renderTodos(todos);
+}
+
 function deleteTodo(event) {
     const index = event.target.getAttribute('data-id');
     todos.splice(index, 1);
     renderTodos(todos);
 };
 
-function clearArray(todos) {
-    while (todos.length) {
-        todos.pop();
-    }
+function clearChecked(list) {
+    todos = list.filter(todo => !todo.isCompleted);
+
     renderTodos(todos);
 };
