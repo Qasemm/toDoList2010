e1c00596001298f93498c04a2954fdf85a36f449
Qasem <qaseem.com34@gmail.com>
2019-10-07 23:07:10 -0700

    make line through item

--- todo.js
+++ todo.js
@@ -17,7 +17,7 @@ function renderTodos(todos) {
         const circleFileName = todo.isCompleted ? 'check.png' : 'circle.png';
 
         return '<div class="todo">' +
-            '<img class="cl" src="' + circleFileName + '" data-id="' + index + '" onClick="toggleCheck(event)">' +
+            '<img class="cl" src="' + circleFileName + '" data-id="' + index + '" onClick="toggleCheck(event) + strikeIt() ">' +
             '<span id="span-txt">' + todo.text + '</span>' +
             '<img class="clo" src="cancel-24px.png" data-id="' + index + '" onClick="deleteTodo(event)">' +
             '</div>';
@@ -28,37 +28,31 @@ function renderTodos(todos) {
     document.getElementById("txt").value = "";
     document.getElementById("plus").style.visibility = "hidden";
 }
-
 document.getElementById("txt").addEventListener("keypress", function(ent) {
     if (ent.keyCode === 13) {
         const text = document.getElementById("txt").value;
-
         if (text === '') {
             return;
         }
-
         addTodo(text);
 
         renderTodos(todos);
     }
 });
-
 document.getElementById("plus").addEventListener("click", function() {
     const text = document.getElementById("txt").value;
 
     if (text === '') {
         return;
     }
-
     addTodo(text);
-
     renderTodos(todos);
-
 });
 
 function toggleCheck(event) {
     const index = event.target.getAttribute('data-id');
     todos[index].isCompleted = !todos[index].isCompleted;
+
     renderTodos(todos);
 }
 
@@ -72,4 +66,17 @@ function clearChecked(list) {
     todos = list.filter(todo => !todo.isCompleted);
 
     renderTodos(todos);
+};
+document.getElementById("todos").addEventListener("click", function() {
+    const text = document.getElementById("txt").value;
+});
+
+function strikeIt(todos) {
+    if (iscompleted = true) {
+        el = document.querySelector('#span-txt');
+        el.style.setProperty('text-decoration', 'line-through');
+    } else {
+        todos.style.setProperty('text-decoration', 'none');
+        enderTodos(todo);
+    }
 };
